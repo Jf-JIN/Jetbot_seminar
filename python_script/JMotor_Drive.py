18b6d1b4a40317cae0760e7fd5ea5a5b4b05624f
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-05-26 05:12:52 +0200

    Update JMotor_Drive.py

--- python_script/JMotor_Drive.py
+++ python_script/JMotor_Drive.py
@@ -4,6 +4,30 @@
 
 from JData_Collect import *
 
-class JMotor_Drive_def():
+
+
+# 姿态校正 (内部引用)
+class JPose_Correction():
+    def __init__(self) -> None:
+        pass
+
+
+
+# 电机控制算法 (内部引用)
+class JMotor_Controller():
+    def __init__(self) -> None:
+        pass
+
+
+
+# 滤波器算法 (内部引用)
+class JFilter():
+    def __init__(self) -> None:
+        pass
+
+
+
+# 电机驱动主算法 (外部可调用)
+class JMotor_Drive():
     def __init__(self, input) -> None: # 这里写类的输入(input)
         pass
