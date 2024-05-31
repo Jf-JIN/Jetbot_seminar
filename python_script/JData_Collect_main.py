ec7bdb7196537b2b68cf96d60dedd3052360d689
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-05-31 13:28:46 +0200

    Create JData_Collect_main.py

---
+++ python_script/JData_Collect_main.py
@@ -0,0 +1,29 @@
+
+'''
+该类是数据收集类，主要是相机识别二维码，识别两侧前方的Apritag，处理IMU数据，并将数据进行封装打包
+'''
+
+from JLocation import *
+
+class Imu_Data_Collection():
+    def __init__(self) -> None:
+        pass
+
+
+class Camera_Data_Collection():
+    def __init__(self) -> None:
+        pass
+
+
+
+class JData_Collection():
+    def __init__(self, input) -> None: # 这里写类的输入(input)
+        pass
+    
+    # 四元数转换为欧拉角
+    def quaternion_to_euler(self):
+        pass
+    
+    # 更新数据
+    def update(self):
+        pass
