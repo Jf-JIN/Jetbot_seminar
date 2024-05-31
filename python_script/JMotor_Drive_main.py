63cef89e3856627976b821f339ff857905105657
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-05-31 14:19:29 +0200

    Update JMotor_Drive_main.py

--- python_script/JMotor_Drive_main.py
+++ python_script/JMotor_Drive_main.py
@@ -2,7 +2,7 @@
 该类为电机驱动类， 主要是电机控制，PID或者其他控制算法，过程姿态矫正，(可能也会有滤波器)
 '''
 
-from JData_Collect import *
+from JData_Collect_main import *
 
 
 
@@ -30,4 +30,4 @@ class JFilter():
 # 电机驱动主算法 (外部可调用)
 class JMotor_Drive():
     def __init__(self, input) -> None: # 这里写类的输入(input)
-        self.left_whell = None
+        pass
