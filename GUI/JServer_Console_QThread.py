17360ecca23101db6393176da333300b17bf938b
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-06-16 20:40:17 +0200

    Update JServer_Console_QThread.py

--- GUI/JServer_Console_QThread.py
+++ GUI/JServer_Console_QThread.py
@@ -1,15 +1,16 @@
 
-from PyQt5.QtCore import QObject, QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal
+
 
 import re
-import subprocess
-import traceback
-import functools
-import signal
-import select
-import time
 import os
 import pty
+import time
+import signal
+import psutil
+import traceback
+import functools
+import subprocess
 
 
 class JConsole_Read_QThread(QThread):
@@ -17,12 +18,11 @@ class JConsole_Read_QThread(QThread):
     signal_finished = pyqtSignal()
     signal_error_output = pyqtSignal(dict)
     signal_running_flag = pyqtSignal(bool)
-    signal_roscore_pid = pyqtSignal(str)
 
     def __init__(self, flag_widget, master_fd) -> None:
         super().__init__()
         self.master_fd = master_fd
-        self.flag_widget = flag_widget
+        self.key_command = flag_widget
         self.flag_running = True
 
     def convert_to_plain_text(self, output):
@@ -33,24 +33,18 @@ class JConsole_Read_QThread(QThread):
     def run(self):
         while self.flag_running:
             try:
-                # r, _, _ = select.select([self.master_fd], [], [], 0.1)
-                # if r:
-                #     output_line = os.read(self.master_fd, 1024).decode(errors='ignore')
                 output_line = os.read(self.master_fd, 1024).decode()
                 if output_line == '':
                     break
                 if output_line:
                     output_line = self.convert_to_plain_text(output_line)
-                    if 'process[master]: started with pid' in output_line:
-                        pid = output_line.split('[')[2].split(']')[0]
-                        self.signal_roscore_pid.emit(pid)
-                        # print('[pid]: ', pid)
                     # print('[output_line]:\t', repr(output_line))
-                    # print('[output_line]:\t', output_line)
-                    self.signal_console_line.emit({self.flag_widget: output_line.strip()})
+                    print('[output_line]:\t', output_line)
+                    self.signal_console_line.emit({self.key_command: output_line.strip()})
             except OSError as e:
-                self.signal_error_output.emit({self.flag_widget: str(e)})
+                self.signal_error_output.emit({self.key_command: str(e)})
                 break
+        self.signal_console_line.emit({self.key_command: '结束所有命令显示'})
         self.signal_running_flag.emit(False)
         self.signal_finished.emit()
 
@@ -59,19 +53,22 @@ class JConsole_Read_QThread(QThread):
         if self.master_fd:
             os.close(self.master_fd)
 
+# 主线程
+
 
 class JConsole_QThread(QThread):
     signal_console_line = pyqtSignal(dict)
     signal_finished = pyqtSignal()
     signal_error_output = pyqtSignal(dict)
     signal_running_flag = pyqtSignal(bool)
+    signal_video_watcher_init = pyqtSignal()
 
-    def __init__(self, flag_widget: str, init_command: dict):
+    def __init__(self, key_command: str, value_command: dict):
         super().__init__()
-        self.init_command = init_command
+        self.value_command = value_command
         self.formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
         self.signal_running_flag.emit(True)
-        self.flag_widget = flag_widget
+        self.key_command = key_command
         self.process = None
         self.pid = None
 
@@ -83,29 +80,25 @@ class JConsole_QThread(QThread):
             self.process.stdin.write(command + '\n')
             self.process.stdin.flush()
 
-    def set_pid(self, pid):
-        self.pid = pid
-        # print('[pid]: ', self.pid)
-
     def run(self):
-        full_command = self.init_command  # 具体命令
+        full_command = self.value_command
         try:
             master_fd, slave_fd = pty.openpty()
-            self.process = subprocess.Popen(full_command, shell=True, stdout=slave_fd, stderr=subprocess.STDOUT, universal_newlines=True)
+            self.process = subprocess.Popen(full_command, shell=True, stdin=subprocess.PIPE, stdout=slave_fd, stderr=subprocess.STDOUT, universal_newlines=True)
             os.close(slave_fd)
-            # self.console_thread = JConsole_Read_QThread(self.flag_widget, master_fd)
-            # self.console_thread.signal_console_line.connect(functools.partial(self.signal_extend, self.signal_console_line))
-            # self.console_thread.signal_finished.connect(functools.partial(self.signal_extend, self.signal_finished))
-            # self.console_thread.signal_error_output.connect(functools.partial(self.signal_extend, self.signal_error_output))
-            # self.console_thread.signal_running_flag.connect(functools.partial(self.signal_extend, self.signal_running_flag))
-            # self.console_thread.signal_roscore_pid.connect(self.set_pid)
-            # self.console_thread.start()
-            # self.console_thread.wait()  # 等待读取线程完成
-            # self.signal_finished.emit()
+            self.console_thread = JConsole_Read_QThread(self.key_command, master_fd)
+            self.console_thread.signal_console_line.connect(functools.partial(self.signal_extend, self.signal_console_line))
+            self.console_thread.signal_finished.connect(functools.partial(self.signal_extend, self.signal_finished))
+            self.console_thread.signal_error_output.connect(functools.partial(self.signal_extend, self.signal_error_output))
+            self.console_thread.signal_running_flag.connect(functools.partial(self.signal_extend, self.signal_running_flag))
+            self.console_thread.start()
+            self.signal_video_watcher_init.emit()
+            self.console_thread.wait()
+            self.signal_finished.emit()
         except subprocess.CalledProcessError as e:
             e = traceback.format_exc()
-            self.signal_error_output.emit({self.flag_widget: f"[JConsole_QThread]: [{self.flag_widget}] - {self.formatted_time} \n{e}"})
-            print(f"[JConsole_QThread]: [{self.flag_widget}] - {self.formatted_time} \n{e}")
+            self.signal_error_output.emit({self.key_command: f"[JConsole_QThread]: [{self.key_command}] - {self.formatted_time} \n{e}"})
+            print(f"[JConsole_QThread]: [{self.key_command}] - {self.formatted_time} \n{e}")
         finally:
             self.cleanup()
 
@@ -113,19 +106,34 @@ class JConsole_QThread(QThread):
         if self.process:
             try:
                 if self.process.stdout:
-                    self.process.stdout.close()  # 关闭标准输出
+                    self.process.stdout.close()
                 if self.process.stderr:
-                    self.process.stderr.close()  # 关闭标准错误输出
-                self.process.terminate()  # 终止子进程
-                self.process.wait()  # 等待子进程终止
+                    self.process.stderr.close()
+                self.process.terminate()
+                self.process.wait()
             except Exception as e:
-                print(f"[JConsole_QThread]: [{self.flag_widget}][停止子线程失败] - {self.formatted_time} \n{e}")
+                print(f"[JConsole_QThread]: [{self.key_command}][停止子线程失败] - {self.formatted_time} \n{e}")
             finally:
                 self.process = None
 
+    def get_process_id(self):
+        for process_element in psutil.process_iter(['pid', 'name']):
+            try:
+                if self.key_command == 'roscore':
+                    if self.key_command in process_element.info['name']:
+                        print('roscore')
+                        return process_element.pid
+                elif self.value_command in process_element.info['name']:
+                    print('ros', process_element.pid)
+                    return process_element.pid
+                else:
+                    return self.process.pid
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        return None
+
     def stop_command(self):
-        print('收到结束程序信号')
-        command = f'kill {self.pid}'
-        print('kill', command)
-        self.process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        self.cleanup()
+        pid = self.get_process_id()
+        if pid:
+            os.kill(pid, signal.SIGTERM)
+            self.cleanup()
