36dccd6ba33a5528da85bf3bbca711dbfcaff11b
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-06-16 20:40:19 +0200

    Update JServer_Function.py

--- GUI/JServer_Function.py
+++ GUI/JServer_Function.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from PyQt5.QtGui import QCloseEvent
-from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QMainWindow
+# from PyQt5.QtGui import
+# from PyQt5.QtCore import
 
 import sys
 import socket
@@ -8,6 +8,7 @@ import socket
 from JClient_Server_Video import *
 from JClient_Server_Console import *
 from JServer_Console_QThread import *
+from JServer_Camera_Listener_QThread import *
 
 
 class JServer_Function(QMainWindow):
@@ -42,71 +43,85 @@ class JServer_Function(QMainWindow):
     # 服务器初始化
     def server_init(self):
         ip = self.get_local_ip()
-        self.server_video = Server_Vedio_QThread(ip)
-        self.server_video.signal_connected_list.connect(self.get_console_connected_list)    # 获取 Video 服务器中的客户端列表
-        self.signal_data_video_send.connect(self.server_video.send_img_all)                 # 向所有客户端发送视频数据
+        self.server_video = Server_Video_QThread(ip)
+        self.server_video.signal_connected_list.connect(self.get_video_connected_list)    # 获取 Video 服务器中的客户端列表
         self.server_video.signal_final_close.connect(self.close_server_by_client)          # 接收客户端传来的关闭信号
-        # self.signal_close_server_send.connect(self.server_video.send_all)                   # 向所有客户端发送关闭客户端的信号
+        self.signal_data_video_send.connect(self.server_video.send_img_all)                 # 向所有客户端发送视频数据
+        self.signal_close_server_send.connect(self.server_video.send_all)                   # 向所有客户端发送关闭客户端的信号
         self.server_video.start()
 
         self.server_console = Server_Console_QThread(ip)
-        self.server_console.signal_data_console_recv.connect(self.console_unpacking)            # 获取客户端发送来的命令行信号
         self.server_console.signal_connected_list.connect(self.get_console_connected_list)      # 获取 Console 服务器中的客户端列表
+        self.server_console.signal_data_console_recv.connect(self.console_unpacking)            # 获取客户端发送来的命令行信号
         self.server_console.signal_final_close.connect(self.close_server_by_client)            # 接收客户端传来的关闭信号
         self.signal_close_server_send.connect(self.server_console.send_all)                     # 向所有客户端发送关闭服务端的信号
         self.server_console.start()
-
-    # 服务器发送视频信号
-    # def send_video(self, video_data):
-    #     self.signal_data_video_send.emit(video_data)
-    def send_video(self):
-        while 1:
-            self.signal_data_video_send.emit(self.video_data)
-            print('发送图片')
-            time.sleep(2)
 
 # **************************************** 命令行功能 ****************************************
     # 解包命令行信号
     @pyqtSlot(dict)
     def console_unpacking(self, data: dict):
+        print(f'[执行解包]: {data}')
+        key_list = [
+            'camera_listener', 'roscore', 'ros_camera', 'ros_rectify',
+            'ros_apriltag_detection', 'ros_imu', 'ros_motor', 'ros_algorithm'
+        ]
+        key = None
+        for item in key_list:
+            if item in data:
+                key = item
+                break
+        self.console_threading_sort(key, data)
 
-        print('roscore' in data)
-        # print('roscore' in self.active_threads)
-        # print(data['roscore'] == 'Emergency_Stop')
-        # print('roscore' in self.active_threads and data['roscore'] == 'Emergency_Stop')
-        # print(f'[console_unpacking]: {data}')
-
-        if 'roscore' in data:
-
-            if 'roscore' not in self.active_threads and data['roscore'] != 'Emergency_Stop':
-                self.concole_thread('roscore', data)
-            elif 'roscore' in self.active_threads and data['roscore'] == 'Emergency_Stop':
-                print('启动结束程序')
-                self.active_threads['roscore'].stop_command()
+    def console_threading_sort(self, key, data):
+        if not key:
+            return
+        print(f'[执行解包分类]: {key in data}, {key}, {data}')
+        if key == 'camera_listener':
+            self.camera_listener_init()
+        elif key not in self.active_threads and data[key] != 'Emergency_Stop':
+            self.concole_thread(key, data)
+        elif key in self.active_threads and data[key] == 'Emergency_Stop':
+            self.active_threads[key].stop_command()
+        elif key in self.active_threads:
+            self.active_threads[key].send_command_to_subprocess(data[key])
 
     # 构建命令行线程
     def concole_thread(self, key, data):
+        print(f'[执行命令行线程初始化]: {data}')
         thread_under_concole_thread = JConsole_QThread(key, data[key])
         thread_under_concole_thread.signal_console_line.connect(self.server_console.send_all)
         thread_under_concole_thread.signal_finished.connect(lambda: self.thread_finish(key))
-        # thread_under_concole_thread.
+        # thread_under_concole_thread.signal_video_watcher_init.connect(self.camera_listener_init)
         thread_under_concole_thread.start()
         self.active_threads[key] = thread_under_concole_thread  # 保持对活动线程的引用
-        print('[命令行线程][启动之后active_thread]: ', self.active_threads)
+        print('[命令行线程] [启动之后active_thread]: ', self.active_threads)
 
     # 结束命令行线程
     def thread_finish(self, key):
-        print('[命令行线程] 结束命令行线程')
+        print('[命令行线程]: 结束命令行线程')
         sender_thrd = self.sender()
-        print(f'[命令行线程][sender类型]: {type(sender_thrd)}')
+        print(f'[命令行线程] [sender类型]: {type(sender_thrd)}')
         sender_thrd.wait()
         sender_thrd.quit()  # 停止线程的事件循环
-        print('[命令行线程][删除之前active_thread]: ', self.active_threads)
+        print('[命令行线程] [删除之前active_thread]: ', self.active_threads)
         if key in self.active_threads:
             self.active_threads.pop(key)  # 删除对象的引用
         else:
             print(f"[命令行线程]: 线程键名 {key} 不存在于 active_threads 中")
-        print('[命令行线程][删除之后active_thread]: ', self.active_threads)
+        print('[命令行线程] [删除之后active_thread]: ', self.active_threads)
+
+    # 初始化启动视频监控
+    def camera_listener_init(self):
+
+        rospy.init_node('listener', anonymous=True)
+        self.vlisten = Camera_Listener_QThread()
+        self.vlisten.video_signal.connect(self.send_video)
+        self.vlisten.start()
+
+    # 服务器发送视频信号
+    def send_video(self, video_data):
+        self.signal_data_video_send.emit(video_data)
 
 # **************************************** 关闭服务器 ****************************************
 
@@ -121,9 +136,9 @@ class JServer_Function(QMainWindow):
 
     # 从客户端关闭服务器
     def close_server_by_client(self):
-        print('[Server] 关闭服务器')
         self.close_client_socket()
         # 此处关闭其他的线程
+        print('[服务器端口]: 关闭服务器')
         sys.exit()
 
     # 关闭所有的客户端Socket
@@ -136,7 +151,7 @@ class JServer_Function(QMainWindow):
             for cclient in self.console_clients_list:
                 csocket: socket.socket = cclient[0]
                 csocket.close()
-        print('[Server] 关闭客户socket')
+        print('[服务器端口] 断开所有客户端socket')
 
     # 服务器关闭事件
     def closeEvent(self, event):
