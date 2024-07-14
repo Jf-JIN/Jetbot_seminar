c4d2045f97b5ac1d131d97aefe2da81453709d58
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-07-15 01:52:25 +0200

    Update JAlgorithm_JPath_Finding.py

--- python_script/JAlgorithm_JPath_Finding.py
+++ python_script/JAlgorithm_JPath_Finding.py
@@ -1,9 +1,9 @@
 '''
 JPath_Finding_A_Star
-    主方法: 
+    主方法:
         用于建立节点树, 并返回节点树
         get_node_tree(start_point: float|int, goal_point: float|int, map_matrix: JMap_Grid_Matrix_From_Yaml) -> list[JPath_Node]
-    其他方法: 
+    其他方法:
         用于将坐标系转换为矩阵索引
         from_coordinate_to_index(coordinate: tuple[float|int, float|int] | list[float|int, float|int], map_matrix: JMap_Grid_Matrix_From_Yaml) -> tuple[int]
         用于将矩阵索引转换为坐标系, 点为方格中点
@@ -14,6 +14,7 @@ from JAlgorithm_JPath_Tree import *
 from JAlgorithm_JPath_Action import *
 
 # A*算法类
+log_info = logger_dict_algo['info']
 
 
 class JPath_Finding_A_Star():
@@ -51,11 +52,13 @@ class JPath_Finding_A_Star():
                 old_vector = new_vector
                 action.set_end_node(new_node)
                 continue
+            # 方向相同，延长终点
             if self.get_angle(old_vector) == self.get_angle(new_vector):
                 action.set_end_node(new_node)
                 # action.set_mode('line')
                 old_node = new_node
                 old_vector = new_vector
+            # 方向不同，转弯节点，保存之前的节点
             else:
                 # action.set_end_node(node_tree[index-2])  # 这里看是否退后一个节点，使得转弯处连贯
                 path_list.append(action)
@@ -68,38 +71,37 @@ class JPath_Finding_A_Star():
                 action = JAction()
                 # action.set_start_node(new_node)
                 action.set_start_node(old_node)
-                # print('拐点:', old_node.current_index)
                 action.set_end_node(new_node)
                 old_node = new_node
                 old_vector = new_vector
+            # 最后节点
             if index == len(node_tree) - 1:
                 action.set_end_node(item)
                 path_list.append(action)
-        # print(len(path_list))
-        # print(path_list)
+        # log_info(len(path_list))
+        # log_info(path_list)
         temp_path = []
         for index, i in enumerate(path_list):
-            # print('\ncurrent:\t', i.end_node.current_index)
-            # print('increase:\t', i.index_x_increase, i.index_y_increase)
             i: JAction
             # 寻找目标墙
             goal_wall = None
+            # 纵向延伸寻找
             if i.index_x_increase:
                 x_index = i.end_node.current_index[0]
                 while not goal_wall:
                     x_index += i.index_x_increase
                     goal_wall_index = [x_index, i.end_node.current_index[1]]
-                    goal_wall: JWall = map_matrix.map_obj_matrix[x_index][i.end_node.current_index[1]]
-                distance = abs(x_index - i.end_node.current_index[0] - i.index_x_increase) * goal_wall.width + goal_wall.width / 2
+                    log_info(goal_wall_index)
+                    goal_wall: JWall = map_matrix.map_obj_matrix[goal_wall_index[0]][goal_wall_index[1]]
+                distance = abs(x_index - i.end_node.current_index[0] - i.index_x_increase) * goal_wall.width / 2 + goal_wall.width / 2
+            # 横向延伸寻找
             elif i.index_y_increase:
                 y_index = i.end_node.current_index[1]
                 while not goal_wall:
                     y_index += i.index_y_increase
                     goal_wall_index = [i.end_node.current_index[0], y_index]
-                    goal_wall: JWall = map_matrix.map_obj_matrix[i.end_node.current_index[0]][y_index]
-                distance = abs(y_index - i.end_node.current_index[1] - i.index_y_increase) * goal_wall.width + goal_wall.width / 2
-            # print('goal_wall\t', goal_wall_index, goal_wall)
-            # print('distance\t', distance)
+                    goal_wall: JWall = map_matrix.map_obj_matrix[goal_wall_index[0]][goal_wall_index[1]]
+                distance = abs(y_index - i.end_node.current_index[1] - i.index_y_increase) * goal_wall.width / 2 + goal_wall.width / 2
             if i.index_x_increase == 1 or i.index_y_increase == 1:  # 向右向下，看到的id是向左向上
                 id_list = [goal_wall.id_list[0], goal_wall.id_list[1]]
             if i.index_x_increase == -1 or i.index_y_increase == -1:    # 向左向上，看到的id是向右向下
@@ -125,9 +127,9 @@ class JPath_Finding_A_Star():
                 'end_orientation': end_orientation
             }
             temp_path.append(temp_action_dict)
-            # print(i,  i.vector, i.path_length, i.orientation, i.start_node.current_index, i.end_node.current_index)
+        log_info('[分解路径]')
         for i in temp_path:
-            print(i)
-        print('\n')
-        print(temp_path)
+            log_info(i)
+        log_info('[路径字典]')
+        log_info(temp_path)
         return temp_path
