124426a9428519a352cd01b7d89c25bc13440e63
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-07-15 01:51:49 +0200

    Update JServer_Function.py

--- python_script/JServer_Function.py
+++ python_script/JServer_Function.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow
 # from PyQt5.QtGui import
 # from PyQt5.QtCore import
 
+import os
 import sys
 import yaml
 import socket
@@ -12,17 +13,27 @@ from JClient_Server_Video import *
 from JClient_Server_Console import *
 from JServer_Console_QThread import *
 from JServer_Apriltag_QThread import *
+from JMotor_Drive_main import *
+from JAlgorithm_JPath_Search import *
+
+
+log_info = logger_dict_server['info']
+log_error = logger_dict_server['error']
 
 
 class JServer_Function(QMainWindow):
     signal_data_video_send = pyqtSignal(object)
     signal_data_console_send = pyqtSignal(dict)
     signal_close_server_send = pyqtSignal(dict)
+    signal_set_pid_distance = pyqtSignal(list)
+    signal_set_pid_angle = pyqtSignal(list)
 
     def __init__(self) -> None:
         super().__init__()
+        os.system('clear')
         self.parameter_init()
         self.server_init()
+        self.motor = None
         # time.sleep(1)
 
     def parameter_init(self) -> None:
@@ -38,7 +49,7 @@ class JServer_Function(QMainWindow):
             local_ip = s.getsockname()[0]
         except Exception as e:
             local_ip = None
-            print(f"Error: {e}")
+            log_error(e)
         finally:
             s.close()
         return local_ip
@@ -60,14 +71,20 @@ class JServer_Function(QMainWindow):
         self.signal_close_server_send.connect(self.server_console.send_all)                     # 向所有客户端发送关闭服务端的信号
         self.server_console.start()
 
+    # 服务器终止，急停电机
+
+    def stop_motor_by_server_close(self, flag):
+        if flag:
+            self.console_unpacking({'motor_action': 'stop'})
+
 # **************************************** 命令行功能 ****************************************
     # 解包命令行信号
     @pyqtSlot(dict)
     def console_unpacking(self, data: dict):
-        print(f'\n[执行解包]: {data}')
+        log_info(f'[执行解包]: {data}')
         key_list = [
-            'camera_listener', 'jlocation_listener', 'ros_node_init', 'a1_map_yaml_dict', 'a1_path_dict', 'roscore', 'ros_camera', 'ros_rectify',
-            'ros_apriltag_detection', 'ros_imu', 'ros_motor', 'ros_algorithm'
+            'camera_listener', 'jlocation_listener', 'ros_node_init', 'a1_map_yaml_dict', 'motor_action', 'a1_path_dict', 'roscore', 'ros_camera', 'ros_rectify',
+            'ros_apriltag_detection', 'ros_imu', 'ros_imu_calib', 'ros_motor', 'ros_algorithm', 'motor_parameter', 'force_accept', 'a2_map_build'
         ]
         key = None
         for item in key_list:
@@ -79,21 +96,63 @@ class JServer_Function(QMainWindow):
     def console_threading_sort(self, key, data):
         if not key:
             return
-        print(f'\n[执行解包分类]: {key in data}, {key}, {data}')
+        log_info(f'[执行解包分类]: {key in data}, {key}, {data}')
         if key == 'camera_listener':
             self.listener_init()
         elif key == 'jlocation_listener':
             self.listener_init()
         elif key == 'ros_node_init':
-            rospy.init_node('jdata_collection_node', anonymous=False)
+            if not rospy.core.is_initialized():
+                rospy.init_node('jdata_collection_node', anonymous=False)
+        elif key == 'motor_action':
+            log_info('here')
+            data_sub = data[key]
+            dict_key = ['mode', 'direction', 'radius', 'degrees', 'id', 'distance']
+            if not isinstance(self.motor, JMotor):
+                self.motor = JMotor(self, self.data_collector, self.server_console.send_all)
+                # self.signal_set_pid_distance.connect(self.motor.signal_set_pid_distance.emit)
+                # self.signal_set_pid_angle.connect(self.motor.signal_set_pid_angle.emit)
+            # 补全字典缺失
+            for item in dict_key:
+                if item not in data_sub:
+                    data_sub[item] = None
+            if data_sub['mode'] == 'stop' and self.motor:
+                log_info('stop')
+                self.motor.stop_motor()
+                log_info('finish')
+            if self.motor.flag_accept:
+                if data_sub['mode'] == 'circle':
+                    log_info('circle')
+                    if data_sub['direction'] and (data_sub['radius'] or data_sub['radius'] == 0) and (data_sub['degrees'] or data_sub['degrees'] == 0):
+                        self.motor.run_circle(direction=data_sub['direction'], time_diff=data_sub['time_diff'], radius=data_sub['radius'], degrees=data_sub['degrees'])
+                    else:
+                        log_info(f'[电机驱动-旋转][参数缺失]: direction:{data_sub["direction"]}, radius:{data_sub["radius"]}, degrees:{data_sub["degrees"]}')
+                elif data_sub['mode'] == 'line':
+                    log_info('line')
+                    if data_sub['id'] and data_sub['distance']:
+                        self.motor.run_line(id=data_sub['id'], distance=data_sub['distance'])
+                    else:
+                        log_info(f'[电机驱动-直行][参数缺失]: id:{data_sub["id"]}, distance:{data_sub["distance"]}')
+        elif key == 'force_accept':
+            if hasattr(self, 'motor'):
+                self.motor.flag_accept = True
+                log_info(f'[电机驱动-强制接收参数]: 当前 self.motor.flag_accept : {self.motor.flag_accept}')
+
         elif key == 'a1_path_dict':
             # 发送信号, 控制小车
-            pass
+            if not isinstance(self.motor, JMotor):
+                self.motor = JMotor(self, self.data_collector, self.server_console.send_all)
+                # self.signal_set_pid_distance.connect(self.motor.signal_set_pid_distance.emit)
+                # self.signal_set_pid_angle.connect(self.motor.signal_set_pid_angle.emit)
+            if self.motor.flag_accept:
+                log_info('[电机驱动]: 执行路径')
+                self.motor.run_path(data[key])
         elif key == 'a1_map_yaml_dict':
-            # print('\n[a1_map_yaml_dict]', data[key])
             loaded_data = data[key]
+            current_time_item = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             if 'standalone_tags' not in loaded_data:
                 loaded_data['standalone_tags'] = data[key]['tag_bundles'][0]['layout']
+            loaded_data = {'time': current_time_item, **loaded_data}
             downloads_path = os.path.join(os.path.expanduser('~'), 'workspace')
             yaml_filename = 'tags.yaml'
             yaml_file_path = os.path.join(downloads_path, yaml_filename)
@@ -101,6 +160,16 @@ class JServer_Function(QMainWindow):
                 yaml.dump(loaded_data, yaml_file, default_flow_style=False, sort_keys=False)
             copy_command = f'echo jetson | sudo -S cp {yaml_file_path} /opt/ros/noetic/share/apriltag_ros/config && sudo -S rm {yaml_file_path}'    # 此处可修改 tags.yaml 的路径
             subprocess.Popen(copy_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        elif key == 'motor_parameter':
+            for sub_key, sub_value in data[key].items():
+                if sub_key == 'pid_distance' and hasattr(self, 'motor') and hasattr(self.motor, 'motor_thread_object') and hasattr(self.motor.motor_thread_object, 'pid_distance'):
+                    self.motor.motor_thread_object.pid_distance.set_kpid(sub_value)
+                elif sub_key == 'motor_calib' and hasattr(self, 'motor') and hasattr(self.motor, 'motor_thread_object'):
+                    self.motor.motor_thread_object.motor_left_calib = sub_value[0]
+                    self.motor.motor_thread_object.motor_right_calib = sub_value[1]
+        elif key == 'a2_map_build':
+            self.path_search = JPath_Search(self.data_collector, self.server_console.send_all)
+            self.path_search.start()
         elif key not in self.active_threads and data[key] != 'Emergency_Stop':
             self.concole_thread(key, data)
         elif key in self.active_threads and data[key] == 'Emergency_Stop':
@@ -112,28 +181,28 @@ class JServer_Function(QMainWindow):
 
     # 构建命令行线程
     def concole_thread(self, key, data):
-        print(f'\n[执行命令行线程初始化]: {data}')
+        log_info(f'[执行命令行线程初始化]: {data}')
         thread_under_concole_thread = JConsole_QThread(key, data[key])
         thread_under_concole_thread.signal_console_line.connect(self.server_console.send_all)
         thread_under_concole_thread.signal_finished.connect(lambda: self.thread_finish(key))
         # thread_under_concole_thread.signal_video_watcher_init.connect(self.camera_listener_init)
         thread_under_concole_thread.start()
         self.active_threads[key] = thread_under_concole_thread  # 保持对活动线程的引用
-        print('\n[命令行线程] [启动之后active_thread]: ', self.active_threads)
+        log_info('[命令行线程] [启动之后active_thread]: ', self.active_threads)
 
     # 结束命令行线程
     def thread_finish(self, key):
-        print('\n[命令行线程]: 结束命令行线程')
+        log_info('结束命令行线程')
         sender_thrd = self.sender()
-        print(f'\n[命令行线程] [sender类型]: {type(sender_thrd)}')
+        log_info(f'[sender类型]: {type(sender_thrd)}')
         sender_thrd.wait()
         sender_thrd.quit()  # 停止线程的事件循环
-        print('\n[命令行线程] [删除之前active_thread]: ', self.active_threads)
+        log_info('[删除之前active_thread]: ', self.active_threads)
         if key in self.active_threads:
             self.active_threads.pop(key)  # 删除对象的引用
         else:
-            print(f"[命令行线程]: 线程键名 {key} 不存在于 active_threads 中")
-        print('\n[命令行线程] [删除之后active_thread]: ', self.active_threads)
+            log_info(f'[命令行线程]: 线程键名 {key} 不存在于 active_threads 中')
+        log_info('[删除之后active_thread]: ', self.active_threads)
 
     # 检查ROSCORE是否运行
     def is_roscore_running(self):
@@ -156,16 +225,16 @@ class JServer_Function(QMainWindow):
         if not self.is_roscore_running() or not self.is_node_running('/apriltag_ros_continuous_node'):
             tag_init_roscore = not self.is_roscore_running()
             tag_init_apriltag = not self.is_node_running('/apriltag_ros_continuous_node')
-            print(f'\n[jlocation_listener_init] 未打开ROSCORE: {tag_init_roscore} / 未打开Apriltag识别: {tag_init_apriltag}')
+            log_info(f'未打开ROSCORE: {tag_init_roscore} / 未打开Apriltag识别: {tag_init_apriltag}')
             return
         if not self.is_node_running('/jdata_collection_node') and (self.is_roscore_running() and self.is_node_running('/apriltag_ros_continuous_node')):
             rospy.init_node('jdata_collection_node', anonymous=False)
-        if hasattr(self, 'listener_object') or hasattr(self, 'jlocation_listener_Thread'):
-            print('\n[listener_init]: 重复初始化，已忽略')
+        if hasattr(self, 'data_collector') or hasattr(self, 'jlocation_listener_Thread'):
+            log_info('重复初始化，已忽略')
             return
-        self.listener_object = JData_Collection()
-        self.listener_object.video_signal.connect(self.send_video)
-        self.jlocation_listener_Thread = JServer_Apriltag_QThread(self.listener_object)
+        self.data_collector = JData_Collection()
+        self.data_collector.video_signal.connect(self.send_video)
+        self.jlocation_listener_Thread = JServer_Apriltag_QThread(self.data_collector)
         self.jlocation_listener_Thread.signal_jlocation_package.connect(self.server_console.send_all)
         self.jlocation_listener_Thread.signal_current_location.connect(self.server_console.send_all)
         self.jlocation_listener_Thread.start()
@@ -189,7 +258,7 @@ class JServer_Function(QMainWindow):
     def close_server_by_client(self):
         self.close_client_socket()
         # 此处关闭其他的线程
-        print('\n[服务器端口]: 关闭服务器')
+        log_info('关闭服务器')
         sys.exit()
 
     # 关闭所有的客户端Socket
@@ -202,7 +271,7 @@ class JServer_Function(QMainWindow):
             for cclient in self.console_clients_list:
                 csocket: socket.socket = cclient[0]
                 csocket.close()
-        print('\n[服务器端口] 断开所有客户端socket')
+        log_info('断开所有客户端socket')
 
     # 服务器关闭事件
     def closeEvent(self, event):
