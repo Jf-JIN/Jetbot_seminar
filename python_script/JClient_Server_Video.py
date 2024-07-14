f18c739d214408338dbd96ab0ef6a2d75edd2e00
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-07-15 01:51:00 +0200

    Update JClient_Server_Video.py
    更改了调试显示方式

--- python_script/JClient_Server_Video.py
+++ python_script/JClient_Server_Video.py
@@ -11,14 +11,19 @@ from PyQt5.QtCore import pyqtSignal, QThread, pyqtSlot
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtCore import QByteArray
 
+from JLogger import *
+
 # 常量
 DEFAULT_PORT = 10086    # 默认端口
-BUFFER_SIZE = 1024      # 默认缓存大小
+BUFFER_SIZE = 4096      # 默认缓存大小
 TRANSMISSION_FORMAT = '!Q'
+
+logger_dict = JLog('Socket', '视频端口')
+log_info = logger_dict['info']
+log_error = logger_dict['error']
 
 
 def reconnect_on_timeout(func):
-    '''装饰器: 在捕获超时异常时重新连接'''
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):
         max_retries = 10  # 重连次数
@@ -26,10 +31,10 @@ def reconnect_on_timeout(func):
             try:
                 return func(self, *args, **kwargs)
             except socket.timeout:
-                print(f'\n[客户端_视频端口]: 连接超时, 尝试重新连接 ({attempt + 1}/{max_retries})')
+                log_error(f'连接超时, 尝试重新连接 ({attempt + 1}/{max_retries})')
                 self.reconnect()
             except Exception as e:
-                print(f'\n[客户端_视频端口]: 其他错误 ({attempt + 1}/{max_retries}) - {e}')
+                log_error(f'其他错误 ({attempt + 1}/{max_retries}) - {e}')
                 self.reconnect()
         return None
     return wrapper
@@ -40,13 +45,14 @@ class Client_Video_QThread(QThread):
     # signal_data_send = pyqtSignal(object)           # 发送内容信号
     signal_connected_flag = pyqtSignal(bool)        # 连接标签信号
     signal_connected_port = pyqtSignal(str)         # 端口号信号
+    signal_ping = pyqtSignal(str)
     signal_error_output = pyqtSignal(str)           # 报错信号
     signal_server_close = pyqtSignal()              # 服务端将关闭的信号
 
     def __init__(self, ip: str) -> None:
         super().__init__()
         self.formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        print(f'\n[客户端_视频端口] [创建Socket] - {self.formatted_time}')
+        print(f'[创建Socket] - {self.formatted_time}')
         self.ip = ip
         self.flag_running = True
         try:
@@ -68,12 +74,14 @@ class Client_Video_QThread(QThread):
         except OSError as e:
             e = traceback.format_exc()
             self.client_socket.close()
+            self.signal_error_stop_motor.emit()
             return None
 
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[客户端_视频端口] [初始化] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[客户端_视频端口] [初始化] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
+            self.signal_error_stop_motor.emit()
 
     # 发送 客户端发送关闭信号
     @pyqtSlot(dict)
@@ -83,7 +91,8 @@ class Client_Video_QThread(QThread):
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[客户端_视频端口] [发送] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[客户端_视频端口] [发送] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
+            self.client_socket.close()
 
     # 接收 客户端接收服务器发送的视频数据或者文本数据
     @reconnect_on_timeout
@@ -98,13 +107,18 @@ class Client_Video_QThread(QThread):
             try:
                 data = json.loads(data.decode())
                 if 'JPort' in data:
-                    print(f'\n[客户端_视频端口]: 与 {self.server_address} 成功建立连接')
+                    log_info(f'与 {self.server_address} 成功建立连接')
                     self.signal_connected_port.emit(str(data['JPort']))
-                    print(f'\n[客户端_视频端口]: 客户端端口: {data["JPort"]}')
+                    log_info(f'客户端端口: {data["JPort"]}')
                     return None
+                # if data.get('type') == 'time':
+                #     ping_time = (time.time() - data['video_time']) * 1000
+                #     format_ping = int(ping_time)
+                #     log_info(f'延时为[{format_ping}]')
+                #     self.signal_ping.emit(f'{format_ping} ms')
                 if 'server_close' in data:
-                    # print('\n[Client_Console-server_close]:', data)
-                    print(f'\n[客户端_视频端口]: 请求关闭服务器 - {self.formatted_time}')
+                    # log_error(data)
+                    log_info(f'请求关闭服务器 - {self.formatted_time}')
                     self.signal_server_close.emit()
                     return None
             except (UnicodeDecodeError, json.JSONDecodeError):
@@ -125,10 +139,11 @@ class Client_Video_QThread(QThread):
                     data_bytes_img += packet
                 pixmap = self.bytes_to_pixmap(data_bytes_img)  # 将二进制图像数据转换为pixmap, 用于label显示
                 return pixmap
+
             except struct.error:
                 e = traceback.format_exc()
                 self.signal_error_output.emit(f'\n[客户端_视频端口] [接收][数据解包错误] - {self.formatted_time} \n[!错误!] {e}')
-                print(f'\n[客户端_视频端口] [接收][数据解包错误] - {self.formatted_time} \n[!错误!] {e}')
+                log_error(e)
         except ConnectionRefusedError as e:     # [WinError 10061] 由于目标计算机积极拒绝, 无法连接。
             e = traceback.format_exc()
             time.sleep(1)
@@ -144,7 +159,7 @@ class Client_Video_QThread(QThread):
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[客户端_视频端口] [接收] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[客户端_视频端口] [接收] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
             self.flag_running = False
             self.signal_connected_flag.emit(False)  # 发送断开连接的信号
             return None
@@ -161,7 +176,7 @@ class Client_Video_QThread(QThread):
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[客户端_视频端口] [二进制转pixmap] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[客户端_视频端口] [二进制转pixmap] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
 
     # 运行线程
     def run(self) -> None:
@@ -171,13 +186,13 @@ class Client_Video_QThread(QThread):
                 if data:
                     self.signal_connected_flag.emit(True)
                     self.signal_data_video_recv.emit(data)    # 接收数据信号
-                    # print('\n[客户端_视频端口]: 客户端接收: ', data)
+                    # log_info('客户端接收: {data}')
         except:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[客户端_视频端口] [线程运行] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[客户端_视频端口] [线程运行] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
         finally:
-            print('\n[客户端_视频端口]: 关闭客户端')
+            log_info('关闭客户端')
             self.client_socket.close()
 
     # 终止线程
@@ -186,7 +201,7 @@ class Client_Video_QThread(QThread):
         try:
             self.client_socket.shutdown(socket.SHUT_RDWR)  # 关闭 socket 读写操作
         except Exception as e:
-            print(f'\n[客户端_视频端口] [停止] - {self.formatted_time} 关闭 socket 出错: {e}')
+            log_error(f'[停止] {e}')
         self.quit()
 
     # 检查线程是否正在运行
@@ -199,6 +214,7 @@ class Server_Video_QThread(QThread):
     signal_connected_list = pyqtSignal(list)        # 连接设备列表信号
     signal_error_output = pyqtSignal(str)          # 报错信号
     signal_final_close = pyqtSignal()           # 关闭服务器信号
+    signal_error_stop_motor = pyqtSignal()
 
     def __init__(self, ip: str) -> None:
         super().__init__()
@@ -215,20 +231,28 @@ class Server_Video_QThread(QThread):
             self.server_address = (self.ip, DEFAULT_PORT)
             self.server_socket.bind(self.server_address)
             self.server_socket.listen(self.connections_allow_number)
-            print(f'\n[服务器_视频端口]: 服务器在 {self.ip}:{DEFAULT_PORT} 等待连接...')
+            log_info(f'服务器在 {self.ip}:{DEFAULT_PORT} 等待连接...')
         except Exception as e:
             e = traceback.format_exc()
-            self.signal_error_output.emit(f'\n[服务器_视频端口] [初始化] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[服务器_视频端口] [初始化] - {self.formatted_time} \n[!错误!] {e}')
+            self.signal_error_output.emit(e)
+            log_error(f'\n[服务器_视频端口] [初始化] - {self.formatted_time} \n[!错误!] {e}')
+            self.signal_error_stop_motor.emit()
 
     # 发送 服务器向客户端发送端口号
     def send(self, client_socket: socket.socket, message: dict) -> None:
         try:
-            client_socket.sendall(json.dumps(message).encode())
+            # client_socket.sendall(json.dumps(message).encode())
+            data = json.dumps(message).encode()
+            # size: bytes = struct.pack(TRANSMISSION_FORMAT, len(data))
+            # type_header = b'\x03'  # 假设 \x02 表示时间戳数据类型
+            # client_socket.sendall(type_header + size + data)
+            client_socket.sendall(data)
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[服务器_视频端口] [发送] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[服务器_视频端口] [发送] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
+            self.signal_error_stop_motor.emit()
+            client_socket.close()
 
     # 向所有设备发送信息
     def send_all(self, message: dict) -> None:
@@ -249,7 +273,9 @@ class Server_Video_QThread(QThread):
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[服务器_视频端口] [发送图片] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[服务器_视频端口] [发送图片] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
+            self.signal_error_stop_motor.emit()
+            client_socket.close()
 
     # 向所有设备发送信息
     def send_img_all(self, img_info: list) -> None:
@@ -259,11 +285,9 @@ class Server_Video_QThread(QThread):
 
     # 删除clients_list中的元素
     def clients_list_remove(self, client: list) -> None:
-        # print('\n[服务器_视频端口]: 之前\t', self.clients_list)
         if client in self.clients_list:
             self.clients_list.remove(client)
-        # print('\n[服务器_视频端口]: 之后\t', self.clients_list)
-        print('\n[服务器_文本端口] [当前客户端列表]\t', self.clients_list)
+        log_info(f'[当前客户端列表]\t {self.clients_list}')
 
     # 不知道为啥, connect(信号.emit), 信号发送不出去
     # 用于向上中转信号
@@ -272,7 +296,6 @@ class Server_Video_QThread(QThread):
 
     # 向所有客户端广播, 服务器关闭, 断开与服务器的连接, 当没有客户端与服务器相连时, 服务器执行关闭程序
     def close_server_from_clients_command_send(self):
-        # print('\n[close_server_from_clients_command_send] ', len(self.clients_list), self.clients_list)
         close_signal: dict[str, str] = {'server_close': 'Server will be closed'}
         self.send_all(close_signal)
         self.flag_close_server_by_client = True
@@ -286,13 +309,13 @@ class Server_Video_QThread(QThread):
                 if client_socket:
                     # 避免重复连接相同的设备
                     if any(client_address[0] in item[1] for item in self.clients_list):
-                        print('\n[服务器_视频端口]: 重复设备, 拒绝连接')
+                        log_info('重复设备, 拒绝连接')
                         continue
                     text = {'JPort': client_address[1]}         # 初始发送内容
                     self.send(client_socket, text)                                      # 发送连接端口号作为连接标志
                     self.clients_list.append([client_socket, client_address])           # 最后将客户端添加进发送列表中/客户端列表中
-                    print(f'\n[服务器_视频端口] [+]: 接受来自 {client_address} 的连接')
-                    print(f'\n[服务器_视频端口]: 当前已连接客户端数量:{len(self.clients_list)}')
+                    log_info(f'[+]: 接受来自 {client_address} 的连接')
+                    log_info(f'当前已连接客户端数量:{len(self.clients_list)}')
                     # 接收客户端信息
                     recv_thread = Server_Handle_Video_QThread(self, client_socket, client_address)
                     recv_thread.signal_remove_clients.connect(self.clients_list_remove)         # 接收客户端断开后, 从列表中移除的信号
@@ -306,7 +329,8 @@ class Server_Video_QThread(QThread):
             except Exception as e:
                 e = traceback.format_exc()
                 self.signal_error_output.emit(f'\n[服务器_视频端口] [线程运行] - {self.formatted_time} \n[!错误!] {e}')
-                print(f'\n[服务器_视频端口] [线程运行] - {self.formatted_time} \n[!错误!] {e}')
+                log_error(e)
+                self.signal_error_stop_motor.emit()
 
     # 终止线程
     def stop(self) -> None:
@@ -345,7 +369,7 @@ class Server_Handle_Video_QThread(QThread):
         except Exception as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[服务器_视频端口] [线程子线程接收] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[服务器_视频端口] [线程子线程接收] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
             self.flag_running = False
             return None
 
@@ -357,26 +381,26 @@ class Server_Handle_Video_QThread(QThread):
                 if not data:
                     continue
                 else:
-                    print(f'\n[*] [服务器_视频端口]: 来自 {self.client_address} 的消息: \n{data}\n')
+                    log_info(f'[*] 来自 {self.client_address} 的消息: {data}')
                     self.signal_data_video_recv.emit(data)    # 数据信号
                     self.signal_connected_list.emit(self.parent_obj.clients_list)
                     if 'close' in data:
-                        print(f'\n[服务器_视频端口]: 客户端 {self.client_address} 请求断开连接')
+                        log_info(f'客户端 {self.client_address} 请求断开连接')
                         break
                     elif 'server_close' in data:
-                        print(f'\n[服务器_视频端口]: 由 {self.client_address} 发出关闭服务器的请求 - {self.formatted_time}')
+                        log_info(f'由 {self.client_address} 发出关闭服务器的请求 - {self.formatted_time}')
                         self.signal_close_server_form_client.emit()  # 向服务器主线程发送信号, 向所有客户端广播
-                        print('\n[服务器_视频端口]: 服务器请求所有客户端断开连接')
+                        log_info('服务器请求所有客户端断开连接')
 
         except ConnectionResetError as e:
             e = traceback.format_exc()
             self.signal_error_output.emit(f'\n[服务器_视频端口] [线程子线程运行] [!错误!] - {self.formatted_time} \n[!错误!] {e}')
-            print(f'\n[服务器_视频端口] [线程子线程运行] - {self.formatted_time} \n[!错误!] {e}')
+            log_error(e)
         finally:
             self.signal_remove_clients.emit([self.client_socket, self.client_address])  # 连接关闭时,从客户端列表中移除断开连接的客户端
             self.signal_connected_list.emit(self.parent_obj.clients_list)  # 发送该客户端断开连接后的客户端列表, 用于实例化服务器主线程的进程获取列表数据
-            print('\n[服务器_视频端口]: 当前已连接客户端数量:', len(self.parent_obj.clients_list))
-            print(f'\n[服务器_视频端口] [-]: 与 {self.client_address} 的连接已关闭')
+            log_info(f'当前已连接客户端数量:{len(self.parent_obj.clients_list)}')
+            log_info(f'[-]: 与 {self.client_address} 的连接已关闭')
             if self.parent_obj.flag_close_server_by_client:
                 self.signal_final_close.emit()
             self.client_socket.close()       # 断开与客户端socket连接
